481aaaffe4a41b9eb8e031be9377017cf8906d64
Your Namehanyuanchi <2540666396@qq.com>
2023-02-09 23:21:47 +0800

    引入ui

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,6 +2,7 @@
   <h1>{{ msg }}</h1>
   <button @click="count++">count is: {{ count }}</button>
   <p>Edit <code>components/HelloWorld.vue</code> to test hot module replacement.</p>
+  <el-button>I am ElButton</el-button>
 </template>
 
 <script>
@@ -16,4 +17,10 @@ export default {
     }
   }
 }
+</script>
+<script>
+  import { ElButton } from 'element-plus'
+  export default {
+    components: { ElButton },
+  }
 </script>
